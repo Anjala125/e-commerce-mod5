ee28f8ff3fee15a47d96ecaf707c539e6839ead8
Anjala <120647099+Anjala125@users.noreply.github.com>
2026-06-01 23:14:37 -0700

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://shopeasy-backend-1252.onrender.com/api"
 })
 
 export default API
